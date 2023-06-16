81dc1f4338c6c64f9ca598cbb515facfaad8a87f
leonTech254 <martinleontech23@gmail.com>
2023-06-16 11:19:11 +0300

    --connceting axios-with vuesjs

---
+++ src/axios.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+axios.defaults.baseURL="https://api.leonteqsecurity.com/"
+
+
+export default axios
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,75 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>{{response}}</h1>
+
+  <div class="projects">
+
+  <ul v-for="(project,index) in AllProjects" :key="index">
+  <li>{{project.title }}</li>
+  <li>{{ project.category }}</li>
+  <li>{{ project.description }}</li>
+  <li>{{ project.price }}</li>
+  <li>{{ project.language }}</li>
+  </ul>
+  
+
+  </div>
+
+
+<button @click="UserDetails">
+click me
+</button>
+ 
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      response:'Hatutaki!!',
+      AllProjects:[]
+    }
+  },
+methods: {
+  UserDetails()
+  {
+    
+    axios.get("fetch_all/project/")
+    .then((res)=>{
+      this.AllProjects=res.data.allProjects
+    })
+
+
+
+  }
+},
+
+
+
+}
+</script>
+
+<style scoped>
+.projects ul
+{
+  display: flex;
+  gap: .5rem;
+  margin-bottom: .5rem;
+
+  
+}
+.projects ul li
+{
+  padding: 6px;
+  background-color: rebeccapurple;
+  width: 10rem;
+  list-style: none;
+  text-align: center;
+}
+
+
+</style>
